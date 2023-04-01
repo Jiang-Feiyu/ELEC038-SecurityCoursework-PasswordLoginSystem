9a4d70c09fc824ed3f16ab03488199d08edd8822
Febian <51969569+FebianFebian1@users.noreply.github.com>
2023-04-01 12:40:02 +0100

    Add files via upload

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,15 @@
-import React, { useState } from 'react';
-import './LoginForm.css';
-import { Link, useNavigate } from 'react-router-dom';
-
-const users = [
-  { "username": "ELEC0138", "password": "Privacy and Security", "Hint": "Apple" },
-  { "username": "USER1234", "password": "MyPassword123", "Hint": "Polar" },
-  { "username": "JOHNDOE", "password": "qwertyuiop", "Hint": "UCL" }
-];
+import React, { useState } from "react";
+import "./LoginForm.css";
+import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [isUsernameSubmitted, setIsUsernameSubmitted] = useState(false);
-  const [hint, setHint] = useState('');
+  const [hint, setHint] = useState("");
   const navigate = useNavigate();
+
+  const users = JSON.parse(localStorage.getItem("users")) || [];
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -28,30 +24,53 @@ function LoginForm() {
     const user = users.find((user) => user.username === username);
 
     if (!user) {
-      alert('user not found');
+      alert("User not found");
     } else {
       setIsUsernameSubmitted(true);
-      setHint(user.Hint);
+      setHint(user.hint);
     }
   };
 
-  const handleLoginFormSubmit = (event) => {
+  const handleLoginFormSubmit = async (event) => {
     event.preventDefault();
     const user = users.find((user) => user.username === username);
 
-    if (user.password !== password) {
-      alert("Password is wrong");
-    } else {
-      navigate('/sucess');
+    const data = {
+      plain_password: password,
+      password_hash: user.password,
+    };
+
+    try {
+      const r = await fetch("http://localhost:5000/encryptions/test", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      const rJson = await r.json();
+
+      if (rJson.password_match) {
+        navigate("/sucess");
+      } else {
+        alert("Invalid password.");
+      }
+    } catch (e) {
+      alert("Invalid password.");
+      console.log(e);
     }
   };
 
   const handleBack = () => {
-    navigate('/');
-  }
+    navigate("/");
+  };
 
   return (
-    <form className="login-form" onSubmit={isUsernameSubmitted ? handleLoginFormSubmit : handleUsernameSubmit}>
+    <form
+      className="login-form"
+      onSubmit={
+        isUsernameSubmitted ? handleLoginFormSubmit : handleUsernameSubmit
+      }
+    >
       <div className="content-box">
         <h2>Login</h2>
         {!isUsernameSubmitted && (
@@ -83,10 +102,11 @@ function LoginForm() {
           </div>
         )}
         <div className="btn-container">
-          <button className="signup-btn" onClick={handleBack}>back
+          <button className="signup-btn" onClick={handleBack}>
+            back
           </button>
           <button type="submit" className="login-btn">
-            {isUsernameSubmitted ? 'Login' : 'Next'}
+            {isUsernameSubmitted ? "Login" : "Next"}
           </button>
         </div>
       </div>
