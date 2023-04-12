282e098815694e59a917d3bb2fa437fcd7870f21
Febian <51969569+FebianFebian1@users.noreply.github.com>
2023-04-12 16:10:04 +0100

    Add files via upload

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./LoginForm.css";
 import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordErrorCount, setPasswordErrorCount] = useState(0);
+  const [delay, setDelay] = useState(5);
   const [isUsernameSubmitted, setIsUsernameSubmitted] = useState(false);
   const [hint, setHint] = useState("");
   const navigate = useNavigate();
@@ -53,9 +55,11 @@ function LoginForm() {
         navigate("/sucess");
       } else {
         alert("Invalid password.");
+        setPasswordErrorCount(passwordErrorCount + 1);
       }
     } catch (e) {
       alert("Invalid password.");
+      setPasswordErrorCount(passwordErrorCount + 1);
       console.log(e);
     }
   };
@@ -64,13 +68,24 @@ function LoginForm() {
     navigate("/");
   };
 
+  const maxLoginAttemptReached = passwordErrorCount >= 5;
+
+  useEffect(() => {
+    let countdown;
+    if (delay <= 0) {
+      clearInterval(countdown);
+      setDelay(5);
+      setPasswordErrorCount(0);
+    }
+
+    if (maxLoginAttemptReached)
+      countdown = setInterval(() => setDelay(delay - 1), 1000);
+
+    return () => clearInterval(countdown);
+  }, [maxLoginAttemptReached, delay]);
+
   return (
-    <form
-      className="login-form"
-      onSubmit={
-        isUsernameSubmitted ? handleLoginFormSubmit : handleUsernameSubmit
-      }
-    >
+    <div className="login-form">
       <div className="content-box">
         <h2>Login</h2>
         {!isUsernameSubmitted && (
@@ -99,18 +114,30 @@ function LoginForm() {
               value={password}
               onChange={handlePasswordChange}
             />
+            {maxLoginAttemptReached && (
+              <div style={{ color: "#ff5555" }}>
+                Maximum login attempt reached! {delay}s
+              </div>
+            )}
           </div>
         )}
         <div className="btn-container">
           <button className="signup-btn" onClick={handleBack}>
             back
           </button>
-          <button type="submit" className="login-btn">
+          <button
+            type="submit"
+            className="login-btn"
+            disabled={maxLoginAttemptReached}
+            onClick={
+              isUsernameSubmitted ? handleLoginFormSubmit : handleUsernameSubmit
+            }
+          >
             {isUsernameSubmitted ? "Login" : "Next"}
           </button>
         </div>
       </div>
-    </form>
+    </div>
   );
 }
 
